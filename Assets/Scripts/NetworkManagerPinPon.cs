52d81d838df52dc1d2b9b9a56ae8b3b56317c151
xupi <antoniogs9@hotmail.es>
2020-06-26 03:26:42 +0200

    boton reinicio y fixed bugs

--- Assets/Scripts/NetworkManagerPinPon.cs
+++ Assets/Scripts/NetworkManagerPinPon.cs
@@ -30,8 +30,14 @@ namespace Mirror.Examples.Pong
 
         public void SpawnPelota(Transform jugador)
         {
-            Vector3 posJugador = jugador.transform.position;
-            ball = Instantiate(spawnPrefabs.Find(prefab => ball), posJugador + new Vector3(0f, 1f, 0.7f), new Quaternion(1f, 1f, 1f, 1f));
+            ball = Instantiate(spawnPrefabs.Find(prefab => ball), jugador.position + new Vector3(0f, 1f, 0.7f), new Quaternion(1f, 1f, 1f, 1f));
+            NetworkServer.Spawn(ball);
+        }
+
+        public void reiniciar(Transform jugador)
+        {
+            NetworkServer.Destroy(ball);
+            ball = Instantiate(spawnPrefabs.Find(prefab => ball), jugador.position + new Vector3(0f, 1f, 0.7f), new Quaternion(1f, 1f, 1f, 1f));
             NetworkServer.Spawn(ball);
         }
 
